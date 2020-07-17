f2e3fe3b5810de8a1bef2c63a1b3ed37a0a2e241
Jeremy Purser <jeremypurser@gmail.com>
2020-07-17 08:26:01 -0400

    Add todo comment

--- index.js
+++ index.js
@@ -8,3 +8,10 @@ $root.addEventListener('click', function (event) {
   console.log('target: ', event.target);
   console.log('id: ', event.target.id);
 });
+
+/**
+ * photos for each medium as background photo
+ * css hover to different photo and show text
+ *
+ * js slideshow
+ */
