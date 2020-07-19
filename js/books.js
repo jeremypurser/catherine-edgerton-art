05c94652df382f9fd25dcbb3e8f260293306ecce
Jeremy Purser <jeremypurser@gmail.com>
2020-07-19 18:14:30 -0400

    Correct photo

--- js/books.js
+++ js/books.js
@@ -1,22 +1,22 @@
 export const booksList = [
   {
     text: '2017 Gifts of Winter (Book 13)',
-    href: '../pages/gifts-of-winter.html',
+    href: 'pages/gifts-of-winter.html',
   },
   {
     text: '2016 For the Road (Book 12)',
-    href: '../pages/for-the-road.html',
+    href: 'pages/for-the-road.html',
   },
   {
     text: '2014-2015 Big Love (Book 11)',
-    href: '../pages/big-love.html',
+    href: 'pages/big-love.html',
   },
   {
     text: '2000-2008: Selected Works from Books 5-10',
-    href: '../pages/sw-5-10.html',
+    href: 'pages/sw-5-10.html',
   },
   {
     text: '1996-2000: Selected Works from Books 1-4',
-    href: '../pages/sw-1-4.html',
+    href: 'pages/sw-1-4.html',
   },
 ];
