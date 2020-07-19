a29a079931f59f9fa9a97ac5a442a33af014cd4d
Jeremy Purser <jeremypurser@gmail.com>
2020-07-19 15:19:44 -0400

    Remove unneeded glass script

--- js/glass.js
+++
@@ -1,7 +0,0 @@
-export const glassList = [
-  '../img/glass-1-preview.jpg',
-  '../img/glass-2-preview.jpg',
-  '../img/glass-3-preview.jpg',
-  '../img/glass-4-preview.jpg',
-  '../img/glass-5-preview.jpg',
-];
