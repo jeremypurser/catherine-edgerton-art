51fdc83a46e97684573a6f62831c14f830010c4e
Jeremy Purser <jeremypurser@gmail.com>
2020-07-18 22:18:15 -0400

    Add glass list

---
+++ js/glass.js
@@ -0,0 +1,7 @@
+export const glassList = [
+  '../img/glass-1-preview.jpg',
+  '../img/glass-2-preview.jpg',
+  '../img/glass-3-preview.jpg',
+  '../img/glass-4-preview.jpg',
+  '../img/glass-5-preview.jpg',
+];
